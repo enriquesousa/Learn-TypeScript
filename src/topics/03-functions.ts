becae3dc4260aafd0803d8fbf98e2892b169a214
Enrique <enrique.sousa@gmail.com>
2025-10-23 06:48:45 -0700

    14. Funciones Básicas

---
+++ src/topics/03-functions.ts
@@ -0,0 +1,44 @@
+
+// Funcion normal
+function addNumbers(a: number,b: number): number {
+    return a+b;
+}
+
+// Funcio de tipo flecha
+// Si queremos regresar en string lo podemos hacer con return (a+b).toString(); pero tambien podemos usar template string con `${a + b}`)
+// Ventajas de una función arrow:   
+//  - El valor de this se hereda del ámbito léxico donde se definió (contexto estático).  
+//  - Podemos omitir corchetes y return en funciones sencillas
+// const addNumbersFlecha = (a: number,b: number): string => {
+//     return `${a + b}`;
+// }
+// La misma funcio arrow omitiendo corchetes y return
+const addNumbersFlecha = (a: number, b: number): string => `${a + b}`;
+
+// Queremos que secondNumber sea opcional y base tenga un numero por defecto
+// El unico numero obligatorio que se el primer numero
+function multiply(firstNumber: number, secondNumber?: number, base: number = 2){
+    return firstNumber * base;
+}
+
+
+
+const result: number = addNumbers(1,2);
+const result2: string = addNumbersFlecha(1,2);
+const multiplyResult: number = multiply(5);
+
+// Impriimir como objeto solo para poder ver mas claro el resultado
+console.log({result, result2, multiplyResult});
+
+
+
+
+
+
+
+
+
+
+
+// Por lo pronto para que no falle el Vite!
+    export {};
