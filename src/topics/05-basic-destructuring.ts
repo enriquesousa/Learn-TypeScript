40d16f017d725849dc1c1c1dd52011878b6ed088
Enrique <enrique.sousa@gmail.com>
2025-10-25 10:56:12 -0700

    18. Destructuración de Arreglos

--- src/topics/05-basic-destructuring.ts
+++ src/topics/05-basic-destructuring.ts
@@ -26,9 +26,9 @@ const audioPlayer: AudioPlayer = {
 // console.log('Duration: ', audioPlayer.songDuration);
 // console.log('Author: ', audioPlayer.details.author);
 
-// ******************** 
-// Usando Descontrccion
-// ********************
+// ********************** 
+// Usando Destructuracion
+// **********************
 
 // Asignar un nuevo song a la clase nadamas para poder distinguir
 const song = 'New Song';
@@ -39,7 +39,32 @@ const { song:anotherSong, songDuration:duration, details} = audioPlayer;
 // Para details, primero descinstruir primero details y luego sacar el autor
 const { author } = details;
 
-console.log("Song: ", anotherSong);
-console.log("Duration: ", duration);
-console.log("Author: ", author);
+// console.log("Song: ", anotherSong);
+// console.log("Duration: ", duration);
+// console.log("Author: ", author);
+
+// *************************** 
+// Destructuracion de Arreglos
+// ***************************
+
+// const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
+// const trunks = dbz[3] || "No hay personaje";
+
+// console.log('Personaje 3: ', dbz[2]);
+// console.log('Personaje 3: ', dbz[3] || 'No hay personaje');
+// console.log('Personaje 3: ', trunks);
+
+// const [p1,p2,trunks]: string[] = ["Goku", "Vegeta", "Trunks"];
+
+// console.log("Personaje 1: ", p1);
+// console.log("Personaje 2: ", p2);
+// console.log("Personaje 3: ", trunks);
+
+// para llegar a Trunks tambien podemos ingnorar p1 y p2
+// const [, , trunks]: string[] = ["Goku", "Vegeta", "Trunks"];
+// console.log("Personaje 3: ", trunks);
+
+// para el caso que no exista Trunks
+const [, , trunks='No encontrado!']: string[] = ["Goku", "Vegeta"];
+console.log("Personaje 3: ", trunks);
 
