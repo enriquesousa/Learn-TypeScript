5e20f85c0998ad56d8ac0f531ebcfb91e8b4e179
Enrique <enrique.sousa@gmail.com>
2025-10-27 11:55:21 -0700

    20. Resolución de la Tarea

--- src/topics/06-function-desstructuring.ts
+++ src/topics/06-function-desstructuring.ts
@@ -1,47 +1,75 @@
 interface Product {
-    description: string;
-    price: number;
+  description: string;
+  price: number;
 }
 
 const phone: Product = {
-    description: 'Nokia A1',
-    price: 150.0,
-}
+  description: "Nokia A1",
+  price: 150.0,
+};
 
 const tablet: Product = {
-    description: 'iPad Air',
-    price: 250.0,
-}
+  description: "iPad Air",
+  price: 250.0,
+};
 
 interface TaxCalculationOptions {
-    tax: number;
-    products: Product[];
+  tax: number;
+  products: Product[];
 }
 
-function taxCalculation( options: TaxCalculationOptions ): number[] {
-    let total = 0;
-    options.products.forEach( product => {
-        total += product.price;
-    })
-    return [total, total * options.tax ];
+// function taxCalculation( options: TaxCalculationOptions ): number[] {
+//     let total = 0;
+//     options.products.forEach( product => {
+//         total += product.price;
+//     })
+//     return [total, total * options.tax ];
+// }
+
+// **********************************
+// Destructurar prodcut
+// **********************************
+
+// Esta es una opcion 1
+// function taxCalculation({ tax, products }: TaxCalculationOptions): [number, number] {
+//   let total = 0;
+//   products.forEach(({ price }) => {
+//     total += price;
+//   });
+//   return [total, total * tax];
+// }
+
+// Esta es la segunda opcion que esta mejor!
+function taxCalculation(options: TaxCalculationOptions): [number, number] {
+  const {tax, products} = options;
+  let total = 0;
+  products.forEach(({ price }) => {
+    total += price;
+  });
+  return [total, total * tax];
 }
 
-const shoppingCart = [phone,tablet];
+const shoppingCart = [phone, tablet];
 const tax = 0.15;
 
-const result = taxCalculation({
-    products: shoppingCart,
-    tax: tax,
-})
+// const result = taxCalculation({
+//     products: shoppingCart,
+//     tax: tax,
+// })
 
-console.log('Total: ', result[0]);
-console.log('Tax: ', result[1]);
+// console.log("Total: ", result[0]);
+// console.log("Tax: ", result[1]);
 
+// **********************************
 // Destructurar TaxCalculationOptions
+// **********************************
 
+const [total, taxTotal] = taxCalculation({
+  products: shoppingCart,
+  tax: tax,
+});
 
+console.log("Total: ", total);
+console.log("Total Tax: ", taxTotal);
 
-
-
-
-export{}
+export {};
