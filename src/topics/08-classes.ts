f69c2f6fd8d9d0909431ecd12368e7ea7df62f7b
Enrique <enrique.sousa@gmail.com>
2025-10-28 08:15:30 -0700

    25. Priorizar composición sobre herencias

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -2,27 +2,39 @@
 // usamos ? si la propiedad puede estar indefinida
 export class Person {
   constructor(
-    public name: string, 
+    public firstName: string,
+    public lastName: string,
     public address: string = 'No Address') 
   {}
 }
 
 // Creamos una nueva clase que extiende de la clase Person
-export class Hero extends Person {
+// export class Hero extends Person {
+//   constructor(
+//     public alterEgo: string,
+//     public age: number,
+//     public realName: string,
+//   ) {
+//     // Se necesita incluir la funcion super() del padre
+//     super( realName, 'New York');
+//   }
+// }
+
+// Nosotros vamos a tratar de darle prioridad a la composición sobre la Herencia.
+export class Hero {
 
   constructor(
     public alterEgo: string,
     public age: number,
     public realName: string,
-  ) {
-    // Se necesita incluir la funcion super() del padre
-    super( realName, 'New York');
-  }
+    public person: Person,
+  ) {}
 
 }
 
 
-
-// const ironman = new Person('Ironman');
-const ironman = new Hero('Ironman',45,'Tony');
+const tony = new Person('Tony', 'Stark', 'New York');
+const ironman = new Hero('Ironman',45,'Tony', tony);
 console.log("Clase Person: ", ironman);
+
+
