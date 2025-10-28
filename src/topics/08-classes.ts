5addc64f866669b1be5cb0e2fdb3f60172e088c1
Enrique <enrique.sousa@gmail.com>
2025-10-27 20:18:45 -0700

    23. Constructor de una clase

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,15 +1,14 @@
 // Definri una Clase
 // usamos ? si la propiedad puede estar indefinida
 export class Person {
-  public name: string;
-  public address: string;
+  // public name: string;
+  // public address: string;
 
-  constructor() {
-    this.name = 'Enrique';
-    this.address = 'Tijuana';
-  }
+  constructor(
+    public name: string, 
+    public address: string = 'No Address') 
+  {}
 }
 
-const ironman = new Person();
-
+const ironman = new Person('Ironman');
 console.log("Clase Person: ", ironman);
