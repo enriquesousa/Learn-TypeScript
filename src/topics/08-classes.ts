57beb233353d7d2fa77fc39ca20c87282525fb96
Enrique <enrique.sousa@gmail.com>
2025-10-28 07:31:51 -0700

    24. Extender una clase

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,14 +1,28 @@
 // Definri una Clase
 // usamos ? si la propiedad puede estar indefinida
 export class Person {
-  // public name: string;
-  // public address: string;
-
   constructor(
     public name: string, 
     public address: string = 'No Address') 
   {}
 }
 
-const ironman = new Person('Ironman');
+// Creamos una nueva clase que extiende de la clase Person
+export class Hero extends Person {
+
+  constructor(
+    public alterEgo: string,
+    public age: number,
+    public realName: string,
+  ) {
+    // Se necesita incluir la funcion super() del padre
+    super( realName, 'New York');
+  }
+
+}
+
+
+
+// const ironman = new Person('Ironman');
+const ironman = new Hero('Ironman',45,'Tony');
 console.log("Clase Person: ", ironman);
